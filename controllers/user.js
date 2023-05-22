1bcca2c95e144e257aaccbde89e91b16256b2ba7
desnos7 <beugredesnosjeremie@gmail.com>
2023-05-22 20:02:09 +0000

    authe

---
+++ controllers/user.js
@@ -0,0 +1,31 @@
+const  user = require("../modele/utilisateur");
+module.exports={
+
+ inscription: async(req,res)=>{
+    try {
+        const login = await user.findEmail(req.body.email)
+    if(login){
+      const logEmail= new user(req.body)
+      const log= await logEmail.save()
+       res.redirect("connexion")
+    }   
+       } catch (error) {
+          console.log(error);
+        res.status(400).send(error)
+       } 
+    },
+
+
+ connexion:async(req,res)=>{
+    try {
+        const logcon= await user.findCon(req.body.email,req.body.password)
+  const authoken= await user.generateAuthTokenAndSave()
+
+        console.log(logcon)
+       res.redirect("espaceClient")
+       } catch (error) {
+          console.log('cklnez', error)
+        res.status(400).send(error)
+       }
+    }
+}   
