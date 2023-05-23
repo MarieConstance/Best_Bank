d41fbb7d8d661bc0ef734ba14e8c2443fb238496
desnos7 <beugredesnosjeremie@gmail.com>
2023-05-23 16:49:31 +0000

    connexion/deconnexion

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const cors = require("cors");
 const firstPage = require("./routes/route");
 
 require("dotenv").config();
+const connectDB  =require('./config/db')
 const app = express();
+app.use(express.json())
+
+connectDB()
 
 app.use(cors());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", firstPage);
+
+
 
 app.use((req,res)=>{
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,47 +1,51 @@
-const  user = require("../modele/utilisateur");
+const user = require("../modele/utilisateur");
 
+exports.inscription = async (req, res) => {
+  try {
+    const login = await user.findEmail(req.body.email);
+    console.log("login : ", login);
 
- exports.inscription= async(req,res)=>{
-    try {
-        const login = await user.findEmail(req.body.email)
-    if(login){
-      const logEmail= new user(req.body)
-      const log= await logEmail.save()
-       res.redirect("connexion")
-    }   
-       } catch (error) {
-          console.log(error);
-        res.status(400).send(error)
-       } 
+    if (!login) {
+      const logEmail = new user(req.body);
+      const log = await logEmail.save();
+      res.redirect("/connexion");
     }
 
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
 
- exports.connexion=async(req,res)=>{
-    try {
-        const logcon= await user.findCon(req.body.email,req.body.password)
-  const authoken= await user.generateAuthTokenAndSave()
+exports.connexion = async (req, res) => {
+  try {
+    const logcon = await user.findCon(req.body.email, req.body.password);
+    const authoken = await user.generateAuthTokenAndSave();
 
-        console.log(logcon)
-       res.redirect("espaceClient")
-       } catch (error) {
-          console.log('cklnez', error)
-        res.status(400).send(error)
-       }
-    }
-  
-    exports.logout= async(req,res)=>{
-      try{
-        req.user.authokens=req.user.authokens.filter((autthoken)=>{
-          return authoken.authoken != req.authoken  
-        })
-      }catch(e){
-        console.log()
-      }
-      await req.user.save()
-      res.send()
-    
-    }
+    console.log(logcon);
+    res.redirect("espaceClient");
+  } catch (error) {
+    console.log("cklnez", error);
+    res.status(400).send(error);
+  }
+};
 
-    exports.dashbordAcceuil=(req, res) => {
-      res.render("dashbord/dashbordAcceuil");
-    }
+exports.logout = async (req, res) => {
+  try {
+    req.user.authokens = req.user.authokens.filter((authoken) => {
+      return authoken.authoken != req.authoken;
+    });
+  } catch (e) {
+    console.log();
+  }
+  await req.user.save();
+  res.send();
+};
+
+exports.dashbordAcceuil = (req, res) => {
+  res.render("dashbord/dashbordAcceuil");
+};
+
+exports.espaceClient = (req, res) => {
+  res.render("espaceClient");
+};
